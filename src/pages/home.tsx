aa1f17e4f7c83f5229ebda36e258bdd84ae4a6db
LucasMtss <lucasrmts@gmail.com>
2025-01-13 23:15:57 -0300

    feat: add home route

---
+++ src/pages/home.tsx
@@ -0,0 +1,35 @@
+import Footer from "@/components/common/Footer";
+import FooterInfos from "@/components/common/FooterInfos";
+import Header from "@/components/common/Header";
+import SectionLinks from "@/components/common/SectionLinks";
+import AdvertiseForFreeSection from "@/components/specific/AdvertiseForFreeSection";
+import AmentiesSection from "@/components/specific/AmentiesSection";
+import CarouselSection from "@/components/specific/CarouselSection";
+import FindAPlaceSection from "@/components/specific/FindAPlaceSection";
+import ForBuyersSection from "@/components/specific/ForBuyersSection";
+import ForCompaniesSection from "@/components/specific/ForCompaniesSection";
+import Hero from "@/components/specific/Hero";
+import PortugalSection from "@/components/specific/PortugalSection";
+import ReasonsSection from "@/components/specific/ReasonsSection";
+import StatesSection from "@/components/specific/StatesSection";
+
+export default function Home() {
+  return (
+   <div className="w-full h-full">
+      <Header />
+      <Hero />
+      <CarouselSection />
+      <FindAPlaceSection />
+      <AmentiesSection />
+      <AdvertiseForFreeSection />
+      <StatesSection />
+      <PortugalSection />
+      <ReasonsSection />
+      <ForBuyersSection />
+      <ForCompaniesSection />
+      <SectionLinks />
+      <FooterInfos />
+      <Footer />
+   </div>
+  );
+}
